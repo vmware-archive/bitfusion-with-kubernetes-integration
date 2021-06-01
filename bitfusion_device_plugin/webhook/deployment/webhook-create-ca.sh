9eda7366cb195e3ee73f4af459932b951dd921a3
haozheng <77625961@qq.com>
2021-06-01 17:22:18 +0800

    feat:Support tkgi (#17)
    * support tkgi deploy
    
    * support tkgi deploy
    
    * support tkgi deploy
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi

---
+++ bitfusion_device_plugin/webhook/deployment/webhook-create-ca.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "8760h"
+    },
+    "profiles": {
+      "server": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": "8760h"
+      }
+    }
+  }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+  "CN": "Kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "CA",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+cat > server-csr.json <<EOF
+{
+  "CN": "admission",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "Kubernetes",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -hostname=bwki-webhook-svc.bwki.svc  -profile=server server-csr.json | cfssljson -bare server
+
+echo "Create secret by local file"
+# create the secret with CA cert and server cert/key
+kubectl create secret generic bwki-webhook-certs \
+        --from-file=key.pem=server-key.pem \
+        --from-file=cert.pem=server.pem \
+        --dry-run -o yaml |
+    kubectl -n bwki apply -f -
