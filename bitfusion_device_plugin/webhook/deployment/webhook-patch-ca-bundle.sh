3d3cbf74725653eb87cb4694fab3f91d12aadf67
haozheng <77625961@qq.com>
2021-06-01 17:22:18 +0800

    feat:Support tkgi (#17)
    * support tkgi deploy
    
    * support tkgi deploy
    
    * support tkgi deploy
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi
    
    * support tkgi

--- bitfusion_device_plugin/webhook/deployment/webhook-patch-ca-bundle.sh
+++ bitfusion_device_plugin/webhook/deployment/webhook-patch-ca-bundle.sh
@@ -10,6 +10,16 @@ if [ -z "${CA_BUNDLE}" ]; then
     CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.ca\.crt}")
 fi
 
+
+if [ "${K8S_PLATFORM}" == 'tkgi' ]; then
+    str=""
+    for line in `cat ca.pem | base64`
+            do
+            str=$str$line
+    done
+    CA_BUNDLE=$str
+fi
+
 export CA_BUNDLE
 
 if command -v envsubst >/dev/null 2>&1; then
